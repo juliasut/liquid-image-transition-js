ed913b5465c5c69a573a69e0b761182bfa7aea4f
iuliia sutygina <81769855+juliasut@users.noreply.github.com>
2021-10-10 23:26:42 -0600

    Add function from hover library

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+  parent: document.querySelector(".distortion"),
+  intensity: 0.2,
+  image1: "./img1.jpg",
+  image2: "./img2.jpg",
+  displacementImage: "./heightMap.png",
+});
